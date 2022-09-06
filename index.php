7739ee542d384fb71ae126763f3e7e6df866713e
Merrickal <89305713+Merrickal@users.noreply.github.com>
2022-09-06 01:28:30 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <script >
+        window.onload(window.location.replace("Ficha-New.html"))
+    </script>
+    <title>Index</title>
+</head>
+<body>
+    
+</body>
+</html>
